43872efb10e18cfc9ff3bb1eea8816bb1a556c01
Katarzyna Tymura <kasiatymura@gmail.com>
2025-12-09 19:00:29 +0100

    config: docker

---
+++ db/init/01-init.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Wymagane zmienne z .env (docker-compose je wstrzyknie do kontenera db)
+: "${USER_DB_NAME?}" "${USER_DB_USER?}" "${USER_DB_PASSWORD?}"
+: "${CATALOG_DB_NAME?}" "${CATALOG_DB_USER?}" "${CATALOG_DB_PASSWORD?}"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
+DO \$\$
+BEGIN
+   IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '${USER_DB_USER}') THEN
+      EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L', '${USER_DB_USER}', '${USER_DB_PASSWORD}');
+   END IF;
+
+   IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '${CATALOG_DB_USER}') THEN
+      EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L', '${CATALOG_DB_USER}', '${CATALOG_DB_PASSWORD}');
+   END IF;
+END
+\$\$;
+
+DO \$\$
+BEGIN
+   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = '${USER_DB_NAME}') THEN
+      EXECUTE format('CREATE DATABASE %I OWNER %I', '${USER_DB_NAME}', '${USER_DB_USER}');
+   END IF;
+
+   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = '${CATALOG_DB_NAME}') THEN
+      EXECUTE format('CREATE DATABASE %I OWNER %I', '${CATALOG_DB_NAME}', '${CATALOG_DB_USER}');
+   END IF;
+END
+\$\$;
+EOSQL
